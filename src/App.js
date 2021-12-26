81c91c6b712b1df970bfc73ab40e13f5288f2594
Amubasshir <lastmubasshir@gmail.com>
2021-12-26 21:53:11 +0600

    first initial one done

--- src/App.js
+++ src/App.js
@@ -2,18 +2,20 @@ import axios from 'axios';
 import { useEffect, useState } from 'react';
 import './App.css';
 import Header from './components/Header';
+import Main from './components/Main';
 import PunkList from './components/PunkList';
 
 
 function App()
 {
-  const [punkListData, setPunkListData] = useState([])
+  const [PunkListData, setPunkListData] = useState([])
+  const [selectedPunk, setSelectedPunk] = useState(0)
   useEffect(() =>
   {
     const getMyNfts = async () =>
     {
       const openseaData = await axios.get('https://testnets-api.opensea.io/assets?asset_contract_address=0x6f48A6D41779FafAefa94Cb6A6F449CC46718e5A&order_direction=asc')
-      console.log(openseaData.data.assets)
+    
       setPunkListData(openseaData.data.assets)
     }
     return getMyNfts()
@@ -21,8 +23,17 @@ function App()
   return (
     <div className="app">
       <Header />
-     
-      <PunkList punkListData={punkListData}/>
+      {PunkListData.length > 0 && (
+        <>
+          <Main
+            punkListData={PunkListData}
+            selectedPunk={selectedPunk}
+          />
+          <PunkList
+            punkListData={PunkListData}
+            setSelectedPunk={setSelectedPunk} />
+          </>
+      )}
     </div>
   );
 }
